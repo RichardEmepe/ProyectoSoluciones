9159939b7bb242846c27f49b69fc801baa87203a
Richard Machoa <richardmp1999@gmail.com>
2023-10-23 14:05:12 -0500

    Inicio web
    Falta imágenes

---
+++ clinica/src/main/java/com/proyecto_clinica/clinica/Controller/InicioController.java
@@ -0,0 +1,14 @@
+package com.proyecto_clinica.clinica.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/inicio")
+public class InicioController {
+    
+    @RequestMapping("/")
+    public String inicio(){
+        return "inicio/index";
+    }
+}

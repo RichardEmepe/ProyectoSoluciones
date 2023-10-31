6206097f6c4f748a066622ccfdd7eeb88a5e80c9
Richard Machoa <richardmp1999@gmail.com>
2023-10-31 01:42:21 -0500

    Interfaz Login implementada
    - Se añadió el controlador LoginController.java
    - Archivo CSS "styleLogin.css"
    - Archivo HTML "login_vm.html"

---
+++ clinica/src/main/java/com/proyecto_clinica/clinica/Controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.proyecto_clinica.clinica.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    
+    @RequestMapping("/")
+    public String login(){
+        return "login/login_vm";
+    }
+    
+}

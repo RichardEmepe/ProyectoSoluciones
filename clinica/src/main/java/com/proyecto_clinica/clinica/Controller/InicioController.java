521fcd778f67130073282ee9a514ff2fd53a791a
Richard Machoa <richardmp1999@gmail.com>
2023-11-20 23:01:26 -0500

    Update InicioController.java

--- clinica/src/main/java/com/proyecto_clinica/clinica/Controller/InicioController.java
+++ clinica/src/main/java/com/proyecto_clinica/clinica/Controller/InicioController.java
@@ -1,9 +1,7 @@
 package com.proyecto_clinica.clinica.Controller;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
-import org.hibernate.type.descriptor.java.LocalDateTimeJavaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
